874e24816607bfaac85ec29c7e30564b7de40bbe
RASHED-R <rashedrazzak7@gmail.com>
2024-06-10 15:30:12 +0600

    forget password done and handle email verification with update user name to firebase

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
@@ -18,9 +18,10 @@ const Register = () => {
     const handleSubmit = e => {
         e.preventDefault();
         const email = e.target.email.value;
+        const name = e.target.name.value;
         const password = e.target.password.value;
         const acceptCheck = e.target.checkbox.checked;
-        console.log("acceptCheck", acceptCheck)
+        console.log(name, email, password, acceptCheck)
 
         setLoginError('');
         setLoginSuccess('');
@@ -36,10 +37,20 @@ const Register = () => {
 
         // console.log('Name: ', name, 'Email: ', email, 'Password: ', password);
         // console.log('first')
+
+        //create User
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 const user = userCredential.user;
                 setLoginSuccess(user)
+                //update user
+
+                updateProfile(user, {
+                    displayName: name,
+                })
+                //send verification email 
+                sendEmailVerification(user)
+                    .then(() => alert('please verify your email'))
             })
             .catch((error) => {
                 const errorMessage = error.message;
@@ -57,7 +68,7 @@ const Register = () => {
                 </p>
                 <form className="max-w-screen-lg mt-8 mb-2 w-80 sm:w-96" onSubmit={handleSubmit}>
                     <div className="flex flex-col gap-6 mb-1">
-                        {/* <h6
+                        <h6
                             className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
                             Your Name
                         </h6>
@@ -66,7 +77,7 @@ const Register = () => {
                                 className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
                             <label
                                 className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
-                        </div> */}
+                        </div>
                         <h6
                             className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
                             Your Email
